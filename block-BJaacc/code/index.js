755e494f3080a9c1df54daa9cf0402b191b46f85
sonu-kumar2001 <sonurocckkerr@gmail.com>
2020-08-21 02:27:02 +0530

    refactored

--- block-BJaacc/code/index.js
+++ block-BJaacc/code/index.js
@@ -17,7 +17,7 @@ students[42] = "The answer to the meaning of life";
 // 9. Using `console.log` print the value of key `42` from the `students` object
 console.log(students[42]);
 // 10. Check the length of object named `students`.
-students.length
+console.log(students.length)
 // 11. Can you define a key of `let or var` in any object? Reason.
 //yes we can do that because a key in object is string
 // 12. Delete the `isAdult` property from the object `students`. Also, print the object before or after deleting the property
